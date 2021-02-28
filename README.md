9ae1125ca8237b25e8a9344b8fb155e6f8065e5d
Nate Catelli <ncatelli@packetfire.org>
2021-02-27 18:30:12 -0600

    Stub Repo (#1)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # typical
-A toy typesystem
+A toy typesystem built to better understand type checkers. This is HEAVILY influenced by the excellent work [RustTyC](https://github.com/Schwenger/RustTyC).
+
+## Warning
+Please nobody use this. This is entirely an experiment to learn more about type checkers.
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! This crate functions as a test/toy implementation of a lattice type checker
+//! heavily inspired by the work Maximilian Schwenger on RustTyC. While the
+//! plan is to use this to support the other first principals projects, I would
+//! heavily advice against anyone attempting to use any part of this.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
