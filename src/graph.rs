083d9273ff2a5f85ee7e416f5d6a1197b867d7ba
Nate Catelli <ncatelli@packetfire.org>
2021-03-02 03:46:52 +0000

    Genericize graph index

--- src/graph.rs
+++ src/graph.rs
@@ -1,72 +1,72 @@
 #[derive(Debug, Default, Clone)]
-struct OrderedSet<T> {
-    v: Vec<T>,
-    s: std::collections::HashSet<T>,
+struct OrderedSet<Idx> {
+    v: Vec<Idx>,
+    s: std::collections::HashSet<Idx>,
 }
 
-impl<T: Eq + std::hash::Hash + Clone> OrderedSet<T> {
-    fn insert(&mut self, value: T) -> bool {
+impl<Idx> OrderedSet<Idx>
+where
+    Idx: Eq + std::hash::Hash + Clone + Copy,
+{
+    fn insert(&mut self, value: Idx) -> Option<Idx> {
         if self.s.insert(value.clone()) {
             self.v.push(value);
-            true
+            Some(value)
         } else {
-            false
+            None
         }
     }
 
-    fn iter(&self) -> std::slice::Iter<T> {
+    fn iter(&self) -> std::slice::Iter<Idx> {
         self.v.iter()
     }
 }
 
-type ID = usize;
 #[derive(Debug, Default, Clone)]
-pub struct Graph<ID>
+pub struct Graph<Idx>
 where
-    ID: Into<usize> + From<usize>,
+    Idx: Into<usize> + From<usize>,
 {
-    upstream_sets: Vec<OrderedSet<ID>>,
-    downstream_sets: Vec<OrderedSet<ID>>,
+    upstream_sets: Vec<OrderedSet<Idx>>,
+    downstream_sets: Vec<OrderedSet<Idx>>,
 }
 
-impl Graph<ID>
+impl<Idx> Graph<Idx>
 where
-    ID: Into<usize> + From<usize>,
+    Idx: Clone + Copy + Eq + std::hash::Hash + Default + Into<usize> + From<usize>,
 {
-    pub fn add_node_mut(&mut self) -> ID {
+    pub fn add_node_mut(&mut self) -> Idx {
         self.upstream_sets.push(OrderedSet::default());
         self.downstream_sets.push(OrderedSet::default());
 
-        ID::from(self.upstream_sets.len() - 1)
+        Idx::from(self.upstream_sets.len() - 1)
     }
 
     #[allow(dead_code)]
-    pub fn add_node(mut self) -> (ID, Self) {
+    pub fn add_node(mut self) -> (Idx, Self) {
         let new_id = self.add_node_mut();
         (new_id, self)
     }
 
     pub fn add_edge_mut(
         &mut self,
-        lhs: ID,
-        rhs: ID,
-        mut new_edges: Vec<(ID, ID)>,
-    ) -> Vec<(ID, ID)> {
+        lhs: Idx,
+        rhs: Idx,
+        mut new_edges: Vec<(Idx, Idx)>,
+    ) -> Vec<(Idx, Idx)> {
         let mut work = vec![(lhs, rhs)];
 
         while let Some((lhs, rhs)) = work.pop() {
-            let (lhs, rhs) = (usize::from(lhs), usize::from(rhs));
-
             // Attempt to insert the rhs into the downstream_set
-            if self.downstream_sets[lhs].insert(rhs) {
-                self.upstream_sets[rhs].insert(lhs);
+            if self.downstream_sets[lhs.into()].insert(rhs).is_some() {
+                self.upstream_sets[rhs.into()].insert(lhs);
                 // Inform the caller that a new edge was added
                 new_edges.push((lhs, rhs));
 
-                for &lhs2 in self.upstream_sets[lhs].iter() {
+                for &lhs2 in self.upstream_sets[lhs.into()].iter() {
                     work.push((lhs2, rhs));
                 }
-                for &rhs2 in self.downstream_sets[rhs].iter() {
+                for &rhs2 in self.downstream_sets[rhs.into()].iter() {
                     work.push((lhs, rhs2));
                 }
             }
@@ -76,7 +76,7 @@ where
     }
 
     #[allow(dead_code)]
-    pub fn add_edge(mut self, lhs: ID, rhs: ID) -> (Self, Vec<(ID, ID)>) {
+    pub fn add_edge(mut self, lhs: Idx, rhs: Idx) -> (Self, Vec<(Idx, Idx)>) {
         let new_edges = self.add_edge_mut(lhs, rhs, Vec::new());
 
         (self, new_edges)
@@ -94,14 +94,14 @@ mod tests {
             acc
         });
 
-        let mut new_edges: Vec<(usize, usize)> = [(0, 3), (1, 3), (2, 3), (3, 4)]
+        let mut new_edges: Vec<(_, _)> = [(0, 3), (1, 3), (2, 3), (3, 4)]
             .iter()
             .fold((graph, vec![]), |(g, _), (upstream, downstream)| {
                 g.add_edge(*upstream, *downstream)
             })
             .1;
 
-        let mut expected: Vec<(usize, usize)> = [0, 1, 2, 3].iter().map(|&lhs| (lhs, 4)).collect();
+        let mut expected: Vec<(_, _)> = [0, 1, 2, 3].iter().map(|&lhs| (lhs, 4)).collect();
 
         new_edges.sort_unstable();
         expected.sort_unstable();
